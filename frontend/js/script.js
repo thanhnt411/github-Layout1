33be9cb3f64c6a8d23ee4ba6bca37ab63bfa8a84
thanhnt411 <darkphoenix4112006@gmail.com>
2025-07-30 16:58:20 +0700

    Thêm js và sửa giao diện

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,6 +1,16 @@
 const mobileMenu = document.getElementById("toggle");
-const nav = document.getElementById("mobile-nav");
+const nav = document.getElementById("nav");
+const iconOpen = document.getElementById("isOpen");
+const iconClose = document.getElementById("isClose");
 mobileMenu.addEventListener("click",function() {
     nav.classList.toggle("active");
+    if(nav.classList.contains("active")) {
+        document.body.classList.add("no-scroll");
+        iconOpen.classList.add("menu-icon-close");
+        iconClose.classList.remove("menu-icon-open");
+    }else {
+        document.body.classList.remove("no-scroll");
+        iconOpen.classList.add("menu-icon-open");
+        iconClose.classList.remove("menu-icon-close");
+    }
 });
-
