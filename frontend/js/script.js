6cecaabe779744af40fe4c5c688674d0cd85a03f
thanhnt411 <darkphoenix4112006@gmail.com>
2025-08-01 10:46:08 +0700

    Thêm style tổng thể Layout

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -42,7 +42,7 @@ gridLoad.addEventListener("click",function() {
       spaceBetween: 30,
       centeredSlides: true,
       autoplay: {
-        delay: 2500,
+        delay: 3000,
         disableOnInteraction: false,
       },
       pagination: {
